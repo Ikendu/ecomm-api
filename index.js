ae0a06547908298f57064e0c097ad2c809d441d0
Chibunud <ndubest56@gmail.com>
2023-11-08 11:15:49 +0100

    add gitignor

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require(`express`)
+const cors = require(`cors`)
+
+const app = express()
+
+app.use(cors())
+
+app.post(`/register`, (req, res) => {
+  res.json(`{ name, email, password }`)
+})
+
+app.listen(4000)
