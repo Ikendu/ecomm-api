6dfa04da0d697368fc71d456be00369db622de2f
Chibunud <ndubest56@gmail.com>
2023-11-09 06:54:15 +0100

    api dependencies

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require(`express`)
 const cors = require(`cors`)
+const { default: mongoose } = require('mongoose')
+const User = require('./models/User')
+const bcrypt = require(`bcryptjs`)
+const jwt = require('jsonwebtoken')
+
+const salt = bcrypt.genSaltSync(10)
+const secretJwt = `fsgsyuewy643873vncxm0q34kjd048,znahfuaoghdfj3400232`
 
 const app = express()
 
 app.use(cors())
+app.use(express.json())
 
-app.post(`/register`, (req, res) => {
-  res.json(` name, email, password testing git again`)
-})
+mongoose.connect(
+  `mongodb+srv://ecomm:11111234Aa@cluster0.cuu14a6.mongodb.net/?retryWrites=true&w=majority`
+)
+
+
 
 app.listen(4000)
+
+//
